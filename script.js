cc16d25c4a67159d47e3e70dbcf022cff6117a9f
David Adeyeye <dadeyeye98@gmail.com>
2022-08-09 20:00:37 -0500

    Computer died, upload of progress

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container');
+
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('gridContainer');
+
+container.appendChild(gridContainer);
+
+function createGridItems(width) {
+    for (i = 0; i < width; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        gridContainer.appendChild(gridItem);
+    }
+}
+
+createGridItems(4);
