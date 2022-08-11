de679cb973a70bfa787ea9f02da13b1d0f5e4037
David Adeyeye <dadeyeye98@gmail.com>
2022-08-10 20:22:07 -0500

    Added button to change grid size based on user input

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const container = document.querySelector('#container');
-
-const gridContainer = document.createElement('div');
-gridContainer.classList.add('gridContainer');
-
-container.appendChild(gridContainer);
-
 function createGridItems(width) {
     for (i = 0; i < width; i++) {
         const gridItem = document.createElement('div');
@@ -13,4 +6,24 @@ function createGridItems(width) {
     }
 }
 
-createGridItems(10000);
+const container = document.querySelector('#container');
+
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('gridContainer');
+
+const dimButton = document.createElement('button');
+dimButton.setAttribute('id', 'dimBtn');
+dimButton.textContent = 'Change grid dimensions';
+
+container.appendChild(dimButton);
+container.appendChild(gridContainer);
+
+createGridItems(625);
+
+dimButton.addEventListener('click', () => {
+    let num = prompt('How many squares per side of the grid?')
+    if(num < 101 && num > 0) {
+        document.documentElement.style.setProperty('--grid-dimension', num)
+        createGridItems(num*num);
+    }
+})
